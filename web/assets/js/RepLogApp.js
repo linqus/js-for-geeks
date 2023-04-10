482a0b00abe3c3d12ac4323c613a8006cb76b235
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-10 11:37:41 +0200

    wraping app and helper in self-executing function to make them "private"

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,57 +1,68 @@
-var RepLogApp = {
-    initialize: function($wrapper) {
-        this.$wrapper = $wrapper;
+(function() {
+    var RepLogApp = {
+        initialize: function($wrapper) {
+            this.$wrapper = $wrapper;
+            Helper.initialize($wrapper);
 
-        this.$wrapper.find('.js-delete-rep-log').on(
-            'click',
-            this.handleRepLogDelete.bind(this)
-        );
+            this.$wrapper.find('.js-delete-rep-log').on(
+                'click',
+                this.handleRepLogDelete.bind(this)
+            );
 
-        this.$wrapper.find('tbody tr').on(
-            'click', 
-            this.handleRowClick.bind(this)
-        );
-    },
+            this.$wrapper.find('tbody tr').on(
+                'click', 
+                this.handleRowClick.bind(this)
+            );
+        },
 
-    updateTotalWeightLifted: function () {
- 
-        this.$wrapper.find('.js-total-weight').html(this._calculateTotalWeight());
-    },
+        updateTotalWeightLifted: function () {
+    
+            this.$wrapper.find('.js-total-weight').html(Helper.calculateTotalWeight());
+        },
 
-    handleRepLogDelete: function (e) {
-        e.preventDefault();
-        $link = $(e.currentTarget);
-        $link.find('.fa')
-            .removeClass('fa-trash')
-            .addClass('fa-spinner')
-            .addClass('fa-spin');
+        handleRepLogDelete: function (e) {
+            e.preventDefault();
+            $link = $(e.currentTarget);
+            $link.find('.fa')
+                .removeClass('fa-trash')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
 
-        var deleteUrl = $link.data('url');
-        var $row = $link.closest('tr');
+            var deleteUrl = $link.data('url');
+            var $row = $link.closest('tr');
 
-        var self = this;
-        $.ajax({
-            url: deleteUrl,
-            method: 'DELETE',
-            success: function() {
-                $row.fadeOut('normal', function() {
-                    $row.remove();
-                    self.updateTotalWeightLifted();
-                });
-            }
-        });
-    },
+            var self = this;
+            $.ajax({
+                url: deleteUrl,
+                method: 'DELETE',
+                success: function() {
+                    $row.fadeOut('normal', function() {
+                        $row.remove();
+                        self.updateTotalWeightLifted();
+                    });
+                }
+            });
+        },
 
-    handleRowClick: function() {
-        console.log('row clicked!');
-    },
+        handleRowClick: function() {
+            console.log('row clicked!');
+        },
 
-    _calculateTotalWeight: function() {
-        var totalWeightLifted = 0;
-        this.$wrapper.find('tbody tr').each(function() {
-            totalWeightLifted += $(this).data('weight');
-        });
 
-        return totalWeightLifted;
     }
-}
+
+    var Helper = {
+        initialize: function($wrapper) {
+            this.$wrapper = $wrapper;
+        },
+
+        calculateTotalWeight: function() {
+            var totalWeightLifted = 0;
+            this.$wrapper.find('tbody tr').each(function() {
+                totalWeightLifted += $(this).data('weight');
+            });
+
+            return totalWeightLifted;
+        }
+    }
+})()
